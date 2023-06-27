14df048e33877fff5088deb681d01e726f9021c5
KMotsepe <94868520+KMotsepe@users.noreply.github.com>
2023-06-27 18:17:17 +0200

    Add files via upload
    yuh

---
+++ app.py
@@ -0,0 +1,84 @@
+import os
+import time
+import json
+import random
+
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from g4f import Model, ChatCompletion, Provider
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+@app.post("/chat/completions")
+async def chat_completions(request: Request):
+    data = await request.json()
+    streaming = data.get('stream', False)
+    model = data.get('model', 'gpt-3.5-turbo')
+    messages = data.get('messages')
+    
+    response = ChatCompletion.create(model=model, stream=streaming, messages=messages)
+    
+    if not streaming:
+        while 'curl_cffi.requests.errors.RequestsError' in response:
+            response = ChatCompletion.create(model=model, stream=streaming, messages=messages)
+
+        completion_timestamp = int(time.time())
+        completion_id = ''.join(random.choices(
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', k=28))
+
+        return {
+            'id': 'chatcmpl-%s' % completion_id,
+            'object': 'chat.completion',
+            'created': completion_timestamp,
+            'model': model,
+            'usage': {
+                'prompt_tokens': None,
+                'completion_tokens': None,
+                'total_tokens': None
+            },
+            'choices': [{
+                'message': {
+                    'role': 'assistant',
+                    'content': response
+                },
+                'finish_reason': 'stop',
+                'index': 0
+            }]
+        }
+
+    async def stream():
+        for token in response:
+            completion_timestamp = int(time.time())
+            completion_id = ''.join(random.choices(
+                'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', k=28))
+
+            completion_data = {
+                'id': f'chatcmpl-{completion_id}',
+                'object': 'chat.completion.chunk',
+                'created': completion_timestamp,
+                'model': 'gpt-3.5-turbo-0301',
+                'choices': [
+                    {
+                        'delta': {
+                            'content': token
+                        },
+                        'index': 0,
+                        'finish_reason': None
+                    }
+                ]
+            }
+
+            yield 'data: %s\n\n' % json.dumps(completion_data, separators=(',', ':'))
+            await asyncio.sleep(0.1)
+
+    return StreamingResponse(stream(), media_type='text/event-stream')
+
+if __name__ == '__main__':
+    import uvicorn
+    config = {
+        'host': '0.0.0.0',
+        'port': 1337
+    }
+    uvicorn.run(app, **config)
